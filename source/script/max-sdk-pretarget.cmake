87a8988e5d98673ea5aaa808d16bbaa8c39d42bc
Robert Ramirez <rob@robtherich.org>
2021-05-14 17:22:16 -0400

    move in max-api scripts

---
+++ source/script/max-sdk-pretarget.cmake
@@ -0,0 +1,3 @@
+set(MAX_SDK_INCLUDES "${CMAKE_CURRENT_SOURCE_DIR}/../../max-sdk/source/c74support/max-includes")
+set(MAX_SDK_MSP_INCLUDES "${CMAKE_CURRENT_SOURCE_DIR}/../../max-sdk/source/c74support/msp-includes")
+set(MAX_SDK_JIT_INCLUDES "${CMAKE_CURRENT_SOURCE_DIR}/../../max-sdk/source/c74support/jit-includes")
